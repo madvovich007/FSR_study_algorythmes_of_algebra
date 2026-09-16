0180c7fcabf32c0c8b2259c0d6473e604591b8e1
madvovich007 <madvovich@gmail.com>
2026-09-16 23:27:12 +0300

    )))))))

--- Stack_class.cpp
+++ Stack_class.cpp
@@ -2,69 +2,89 @@
 
 struct list{
     char value;
-     list * next;
+    list * next;
 };
 
-int ins(list*& head, char value){
+int ins( list** head, char value){
     list* new_list = new list;
     new_list->value = value;
-    new_list -> next = head;
-    head = new_list;
+    if (!*head){
+        new_list -> next = nullptr;
+        *head = new_list;
+        return 1;
+    }
+    new_list -> next = *head;
+    *head = new_list;
     return 0;
 }
 
 
-int pop(list*& head){
-    if (head == nullptr) {
+int pop( list** head){
+    if (*head == nullptr) {
         return -1;
     }
-    list * tmp = head;
-    head = head -> next;
-    delete tmp;
+    list * tmp = *head;
+    *head = (*head) -> next;
+    free(tmp);
     return 0;
+}
+
+
+void printing( list * head){
+    if (head){
+        printing(head->next);
+        if (head->next){
+            printf("; ");
+        }
+        if (head->value>=0){
+            printf("+%d", head->value);
+        }
+        else{
+            printf("%d", head->value);
+        }
+    }
 }
 
 
 int main(){
     int tmp_command = -1;
+    char value;
     list* head = nullptr;
-    std::cin >> tmp_command;
     while (tmp_command != 0){
         std::cin >> tmp_command;
         if (tmp_command == 1){
-            char value;
-            std::cin >> value;
-            ins(head, value);
+            scanf(" %c", &value);
+            ins(&head, value);
         }
         if (tmp_command == 2){
             if (head != nullptr){
-                pop(head);
+                pop(&head);
             }
             else{
-                std::cout << "Stack is empty\n";
+                printf("Stack is empty\n");
             }
         }
         if (tmp_command == 3){
             if (head != nullptr){
-                std::cout << head->value << '\n';
+                printf("%c\n", head->value);
             }
             else{
-                std::cout << "Stack is empty\n";
+                printf("Stack is empty\n");
             }
         }
         if (tmp_command == 4){
             if (head != nullptr){
-                std::cout << "0\n";
+                printf("0\n");
             }
             else{
-                std::cout << "1\n";
+                printf("1\n");
             }
         }
         if (tmp_command == 5){
             while (head != nullptr){
                 list * tmp = head;
                 head = head->next;
-                delete tmp;
+                free (tmp);
             }
         }
     }
@@ -72,7 +92,7 @@ int main(){
     list* next;
     while (tmp){
         next = (*tmp).next;
-        delete tmp;
+        free(tmp);
         tmp = next;
     }
     return 0;
